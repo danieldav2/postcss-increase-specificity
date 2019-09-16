e0815c28baa26288df3074537964e2677fad27c6
danieldav2 <33844422+danieldav2@users.noreply.github.com>
2019-09-16 11:48:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,62 @@
-// postcss-increase-specificity v0.2.2
-//changes
 var postcss = require('postcss');
 var objectAssign = require('object-assign');
-var escapeStringRegexp = require('escape-string-regexp');
+var classRepeat = require('class-repeat');
+var isPresent = require('is-present');
+var hasClass = require('has-class-selector');
 require('string.prototype.repeat');
 
 var CSS_ESCAPED_TAB = '\\9';
 
-
 function increaseSpecifityOfRule(rule, opts) {
-	rule.selectors = rule.selectors.map(function(selector) {
-		// Apply it to the selector itself if the selector is a `root` level component
-		// `html:not(#\\9):not(#\\9):not(#\\9)`
-		if(
-			selector === 'html' ||
-			selector === ':root' ||
-			selector === ':host' ||
-			selector === opts.stackableRoot
-		) {
-			return selector + opts.stackableRoot.repeat(opts.repeat);
-		}
+  rule.selectors = rule.selectors.map(function(selector) {
+    if (
+      selector === 'html' ||
+      selector === ':root' ||
+      selector === ':host' ||
+      selector === opts.stackableRoot
+    ) {
+      return selector + opts.stackableRoot.repeat(opts.repeat);
+    }
+    if (
+      selector.startsWith('[data-spot-im-direction]') ||
+      selector.startsWith('[data-spotim-app]')
+    )
+      return opts.stackableRoot.repeat(opts.repeat) + selector;
 
-		// Otherwise just make it a descendant (this is what will happen most of the time)
-		// `:not(#\\9):not(#\\9):not(#\\9) .foo`
-		return opts.stackableRoot.repeat(opts.repeat) + ' ' + selector;
-	});
-
-	if(opts.overrideIds) {
-		if(
-			// If an id is in there somewhere
-			(new RegExp('#(?!' + escapeStringRegexp(CSS_ESCAPED_TAB) + ')')).test(rule.selector) ||
-			// Or it is an attribute selector with an id
-			(/\[id/).test(rule.selector)
-		) {
-			rule.walkDecls(function(decl) {
-				decl.important = true;
-			});
-		}
-	}
+    return opts.stackableRoot.repeat(opts.repeat) + ' ' + selector;
+  });
 }
 
+module.exports = postcss.plugin('postcss-increase-specificity', function(
+  options
+) {
+  var defaults = {
+    repeat: 2,
+    overrideIds: false,
+  };
+  var opts = objectAssign({}, defaults, options);
 
-// Plugin that adds `:not(#\\9)` selectors to the front of the rule thus increasing specificity
-module.exports = postcss.plugin('postcss-increase-specificity', function(options) {
-	var defaults = {
-		// The number of times `:not(#\\9)` is appended in front of the selector
-		repeat: 3,
-		// Whether to add !important to declarations in rules with id selectors
-		overrideIds: true,
-		// The thing we repeat over and over to make up the piece that increases specificity
-		stackableRoot: ':not(#' + CSS_ESCAPED_TAB + ')'
-	};
+  if (opts.id) {
+    return function IDSpecificity(css) {
+      css.walkRules(function(rule) {
+        var isInsideKeyframes =
+          rule.parent.type === 'atrule' && rule.parent.name === 'keyframes';
 
-	var opts = objectAssign({}, defaults, options);
-
-	return function(css) {
-		css.walkRules(function(rule) {
-			// Avoid adding additional selectors (stackableRoot) to descendant rules of @keyframe {}
-			// i.e. `from`, `to`, or `{number}%`
-			var isInsideKeyframes = rule.parent.type === 'atrule' && rule.parent.name === 'keyframes';
-
-			if(!isInsideKeyframes) {
-				increaseSpecifityOfRule(rule, opts);
-			}
-		});
-	};
+        if (!isInsideKeyframes) {
+          increaseSpecifityOfRule(rule, opts);
+        }
+      });
+    };
+  } else {
+    return function classRepeatPlugin(root) {
+      root.walkRules(function(node) {
+        if (isPresent(node.selectors)) {
+          node.selectors = node.selectors.map(function(selector) {
+            return hasClass(selector) ? classRepeat(selector, opts) : selector;
+          });
+        }
+        return node;
+      });
+    };
+  }
 });
